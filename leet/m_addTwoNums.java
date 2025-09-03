04c7621a316e6d06d35757875ac98f8c9f432689
Harshvardhan_Talokar <harshvardhantalokar@gmail.com>
2025-09-03 18:57:51 +0530

    3 aug

---
+++ leet/m_addTwoNums.java
@@ -0,0 +1,45 @@
+
+
+
+// ou are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+
+public class m_addTwoNums {
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode o1 = new ListNode(0);
+        ListNode out = o1;
+        int sum, carry = 0;
+        while (l1 != null || l2 != null) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            sum = x + y + carry;
+            carry = sum / 10;
+            o1.next = new ListNode(sum % 10);
+            o1 = o1.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry > 0)
+            o1.next = new ListNode(carry);
+
+        return out.next;
+    }
+    
+}
